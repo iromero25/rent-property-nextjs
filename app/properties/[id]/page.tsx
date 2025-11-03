f9df15f41241e350121286998ac83ba4c9d66039
Israel Romero <romero81@yahoo.com>
2025-11-03 19:35:00 +0000

    creating property-related components to be consumed by Property page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,9 @@
-// "use client";
+import Link from "next/link";
+import Property from "@/models/Property";
+import connectDB from "@/config/database";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 
 interface PropertyPageProps {
   params: Promise<{
@@ -7,9 +12,45 @@ interface PropertyPageProps {
 }
 
 export default async function PropertyPage({ params }: PropertyPageProps) {
-  console.log("PropertyPage");
-
+  await connectDB();
   const { id } = await params;
+  const property = await Property.findById(id).lean();
 
-  return <div>Property page {id}</div>;
+  if (!property) {
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    );
+  }
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" />
+            Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+            {/* <!-- Sidebar --> */}
+            {/* <aside className='space-y-4'>
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
