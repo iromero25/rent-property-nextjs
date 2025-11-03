42419c4265fda1d3d8797c7a65cbd48c72cf7f3d
Israel Romero <romero81@yahoo.com>
2025-11-03 14:52:42 +0000

    add the Property and User moongoose models

---
+++ models/Property.ts
@@ -0,0 +1,94 @@
+import { IProperty } from "@/types";
+import { Model, Schema, model, models } from "mongoose";
+
+const PropertySchema = new Schema<IProperty, IProperty>(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: [
+      {
+        type: String,
+      },
+    ],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Property =
+  (models.Property as Model<IProperty>) ||
+  model<IProperty>("Property", PropertySchema);
+
+export default Property;
